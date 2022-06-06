d7f4717c3b617da943a1a5ab35a0f582a04ef3f1
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-06 13:21:16 +0300

    Modified Welcome Page

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,8 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  background-image: url(../src/assets/bg.jpg);
+  overflow: hidden;
 }
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,44 @@
+<template>
+  <section
+    class="text-white inline backdrop-blur backdrop-filter mt-10 bg-black/10 rounded-md shadow-md p-10"
+  >
+    <div class="max-w-3xl mx-auto text-center">
+      <h1
+        class="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400"
+      >
+        Vue Live Chat.
+      </h1>
+
+      <div class="flex justify-evenly m-2">
+        <img src="../assets/vue.svg" alt="Vue.js" />
+        <img src="../assets/firebase.svg" alt="firebase" />
+        <img src="../assets/tailwind.svg" alt="Tailwind" />
+      </div>
+      <p
+        class="max-w-xl mx-auto font-semibold mt-4 sm:leading-relaxed sm:text-xl"
+      >
+        Developed using Vue.js , Firebase and Tailwind.
+      </p>
+
+      <div class="flex flex-wrap justify-center gap-4 mt-8">
+        <a
+          class="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring"
+          href="/get-started"
+        >
+          Sign In
+        </a>
+
+        <a
+          class="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring"
+          href="/about"
+        >
+          Sing Up
+        </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
