181a1460ebe592bd7dc7766bb51504ce5a796169
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-06 12:12:30 +0300

    added tailwind

---
+++ src/views/Welcome.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="text-white bg-gray-900">
+    <div
+      class="max-w-screen-xl px-4 py-32 mx-auto sm:px-6 lg:px-8 lg:h-screen lg:items-center lg:flex"
+    >
+      <div class="max-w-3xl mx-auto text-center">
+        <h1
+          class="text-3xl font-extrabold text-transparent sm:text-5xl bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+        >
+          Vue Live Chat.
+        </h1>
+
+        <p
+          class="max-w-xl mx-auto font-semibold mt-4 sm:leading-relaxed sm:text-xl"
+        >
+          Developed using Vue.js , Firebase and Tailwind.
+        </p>
+
+        <div class="flex flex-wrap justify-center gap-4 mt-8">
+          <a
+            class="block w-full px-12 py-3 text-sm font-medium text-white bg-blue-600 border border-blue-600 rounded sm:w-auto active:text-opacity-75 hover:bg-transparent hover:text-white focus:outline-none focus:ring"
+            href="/get-started"
+          >
+            Sign In
+          </a>
+
+          <a
+            class="block w-full px-12 py-3 text-sm font-medium text-white border border-blue-600 rounded sm:w-auto hover:bg-blue-600 active:bg-blue-500 focus:outline-none focus:ring"
+            href="/about"
+          >
+            Sing Up
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
