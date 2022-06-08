367222db79acaf0d53468418f8eddcd9427127c8
Serhat Ak <72757288+srhtak@users.noreply.github.com>
2022-06-08 21:03:09 +0300

    Message box component in window has been redesigned

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -23,7 +23,11 @@ const formattedDocuments = computed(() => {
       class="w-4/5 backdrop-blur backdrop-filter rounded-md shadow-lg bg-black/20 height overflow-y-scroll overflow-x-hidden p-6"
       v-auto-animate
     >
-      <div v-for="doc in formattedDocuments" :key="doc.id">
+      <div
+        class="border-l-4 p-2 border-l-pink-700 rounded-md shadow-2xl mb-3 border-opacity-30"
+        v-for="doc in formattedDocuments"
+        :key="doc.id"
+      >
         <div class="flex">
           <h1
             class="text-2xl font-bold mr-3 text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400"
@@ -34,7 +38,7 @@ const formattedDocuments = computed(() => {
             {{ doc.message }}
           </h2>
         </div>
-        <h2 class="text-sm text-white">{{ doc.createdAt }} ago</h2>
+        <h2 class="text-xs text-white">{{ doc.createdAt }} ago</h2>
       </div>
     </div>
   </div>
